26be2a8dd71d65299dc386e1c1dc55c4965e5b30
Juan Bautista García Traver <juangarciatraver@gmail.com>
2024-12-21 20:49:05 +0100

    Try of redirections to github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import App from "./App";
+// import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter basename={import.meta.env.DEV ? '/' : '/SpotifyTools/'}>
-      <App />
+    <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <h1>Hello World</h1>
+              <Link to='about'>About Us</Link>
+            </div>
+          }
+        />
+        <Route path='/about' element={<div>About</div>} />
+      </Routes>
     </BrowserRouter>
   </Provider>
 );
