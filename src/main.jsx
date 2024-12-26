3adc53b48d259ac9c80cf63b794dcf6543b6cd2e
Juan Bautista García Traver <juangarciatraver@gmail.com>
2024-12-26 20:33:44 +0100

    Tries to fix routing in github-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
 import { createRoot } from "react-dom/client";
-import { RouterProvider, Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const router = createBrowserRouter(createRoutesFromElements(<Route basename={import.meta.env.DEV ? '/' : '/SpotifyTools/'} path='*' element={<App />} />));
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="*" element={<App />} />),
+  { basename: import.meta.env.DEV ? "/" : "/SpotifyTools/" }
+);
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router}  >
+    <RouterProvider router={router}>
       <App />
     </RouterProvider>
   </Provider>
