fdbffac521251f5fb38322d1a804572bd8dd1f0e
Juan Bautista García Traver <juangarciatraver@gmail.com>
2025-01-10 15:36:09 +0100

    Fixes close button on offcanvas
    It was not hidding the offcanvas.

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -9,22 +9,30 @@ import { useState } from "react";
 // Cómo puedo poner los botones en la navbar centrados en vez de que estén totalmente a la derecha
 
 function NavBar({ user }) {
-  const [showOffcanvas, setShowOffcanvas] = useState(false)
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
   const logout = useLogout();
 
   const handleLogoutButtonClick = () => {
-    hideOffcanvas()
+    hideOffcanvas();
     logout();
   };
 
-  const hideOffcanvas = () => {setShowOffcanvas(false)}
-  const toggleOffcanvas = () => {setShowOffcanvas(!showOffcanvas)}
+  const hideOffcanvas = () => {
+    setShowOffcanvas(false);
+  };
+  const toggleOffcanvas = () => {
+    setShowOffcanvas(!showOffcanvas);
+  };
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary mb-3 fixed-top">
       <Container fluid className="justify-content-start">
         {user && (
-          <Navbar.Toggle aria-controls={`offcanvasNavbar`} className="me-3" onClick={toggleOffcanvas}/>
+          <Navbar.Toggle
+            aria-controls={`offcanvasNavbar`}
+            className="me-3"
+            onClick={toggleOffcanvas}
+          />
         )}
         <Navbar.Brand as={Link} to="/" className="bg-body-tertiary">
           <i className="bi bi-spotify" /> Spotify tools
@@ -36,10 +44,17 @@ function NavBar({ user }) {
             placement="start"
             show={showOffcanvas}
           >
-            <Offcanvas.Header closeButton>
+            <Offcanvas.Header>
               <Offcanvas.Title id={`offcanvasNavbarLabel`}>
                 <i className="bi bi-spotify" /> Spotify tools
               </Offcanvas.Title>
+              <button
+                type="button"
+                className="btn-close text-reset"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+                onClick={hideOffcanvas}
+              ></button>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-start flex-grow-1 pe-3">
